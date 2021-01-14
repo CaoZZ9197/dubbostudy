40d06acd4b39f7b576b243a6b73597ddd6467934
e <919741378@qq.com>
2021-01-14 22:38:47 +0800

    first commit

---
+++ src/main/java/com/example/dubbostudy/DubbostudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubbostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubbostudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubbostudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dubbostudy/DubbostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dubbostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubbostudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
